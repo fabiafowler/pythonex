539ae1a4261acff8e921654bde8dda7557e153cf
fabiafowler <47750640+fabiafowler@users.noreply.github.com>
2020-03-02 20:42:10 +0000

    added another option

--- SEE1.py
+++ SEE1.py
@@ -16,3 +16,24 @@ def SEE1(word, phrase):
             answer = count + answer
         break
     return answer
+
+OR
+word= "hello"
+phrase = "hello hi hi hello"
+
+def SEE1(word, phrase):
+    
+    word = word.lower()
+    new_list = phrase.split()   
+    number = len(new_list)
+
+    
+    for i in range(number+1):
+        count=0
+        if word in new_list():
+            count+=1
+        else:
+            contine
+    return count
+
+    print(count)
